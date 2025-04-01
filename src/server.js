55e146a181ce26b22b546217542aa311c1e0ee35
Jack Carter <jsc8443@g.rit.edu>
2025-04-01 12:52:55 -0400

    slight documentation progress

--- src/server.js
+++ src/server.js
@@ -37,6 +37,7 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// recompile request body
 const parseBody = (request, response, handler) => {
   const body = [];
 
